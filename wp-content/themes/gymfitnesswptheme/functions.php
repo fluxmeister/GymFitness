65ec1d9415cc1b93ca168430de114812304b552a
Zeljko Stevanovic <doctorpauk@gmail.com>
2020-11-25 13:21:29 +0100

    38. Adding a CSS Stylesheet

--- wp-content/themes/gymfitnesswptheme/functions.php
+++ wp-content/themes/gymfitnesswptheme/functions.php
@@ -12,3 +12,12 @@ function gymfitness_menus() {
 
 // Hook
 add_action('init', 'gymfitness_menus' );
+
+
+// Adds Stylesheets and JS Files
+function gymfitness_scripts() {
+    // Normalize CSS
+    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+
+}
+add_action('wp_enqueue_scripts', 'gymfitness_scripts');
