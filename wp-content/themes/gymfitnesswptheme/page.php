569aa0f35f9dea4fa8bceae65a5ba10e5a973dc1
Zeljko Stevanovic <doctorpauk@gmail.com>
2020-11-27 03:43:41 +0100

    48. The Page Template - finished

--- wp-content/themes/gymfitnesswptheme/page.php
+++ wp-content/themes/gymfitnesswptheme/page.php
@@ -1,25 +1,25 @@
 <?php get_header(); ?>
 
-    <main class="container page section">
-        <?php while(have_posts() ): the_post();?>
+    <main class="container page section no-sidebars">
+        <?php while(have_posts() ): the_post(); ?>
             <h1 class="text-center text-primary"><?php the_title(); ?></h1>
 
+            <!-- <?php the_post_thumbnail() ?> -->
             <?php 
-                // check if an image exists
-                if( has_post_thumbnail() ) :
+                // check if an image exist
+                if( has_post_thumbnail() ):
                     the_post_thumbnail();
-                else: 
-                    echo "<p>No image</p>";
+                else:
+                    echo "<p> No image </p>";
                 endif;
             ?>
 
             <div class="text-center">
                 <?php the_content(); ?>
             </div>
-
-            
-
         <?php endwhile; ?>
+    
+        <?php get_template_part('template-parts/page', 'loop'); ?>
     </main>
 
 <?php get_footer(); ?>
