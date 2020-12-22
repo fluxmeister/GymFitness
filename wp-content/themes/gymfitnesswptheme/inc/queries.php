6ea6d4ed01ea1c6f5ea7f80839939ebda5aadbe0
Zeljko Stevanovic <zeljko@vvm.agency>
2020-12-22 12:38:00 +0100

    61. Introducing WP_Query to create custom queries from the database

---
+++ wp-content/themes/gymfitnesswptheme/inc/queries.php
@@ -0,0 +1,21 @@
+<?php 
+
+    function gymfitness_classes_list() { ?>
+        <ul class="classes-list">
+            <?php
+                $args = array(
+                    'post_type' => 'gymfitness_classes', 
+                );
+                // Use WP_Query and append the results into $classes
+                $classes = new WP_Query($args);
+
+                while($classes->have_posts()): $classes->the_post();
+                
+            ?>
+
+            <h3><?php the_title(); ?></h3>
+
+            <?php endwhile; wp_reset_postdata(); ?>
+        </ul>
+    <?php }
+
